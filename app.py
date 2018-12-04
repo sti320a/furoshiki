7ff01b823268657ecc0636e887666eb71a0abf76
sti320a <sti320a@gmail.com>
2018-12-04 23:54:00 +0900

    fixed typo

--- app.py
+++ app.py
@@ -12,15 +12,65 @@ from jinja2 import Environment, FileSystemLoader
 class Shortly(object):
     def __init__(self, config):
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        self.jinja_env = Environment(loader=FileSystemLoader(template_path),autoescape=True)
+        self.url_map = Map([
+            Rule('/', endpoint='new_url'),
+            Rule('/<short_id>', endpoint='follow_short_link'),
+            Rule('/<short_id>+',endpoint='shor_link_details' )
+        ])
     def dispatch_request(self, request):
-        return Response('Hello World')
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, 'on_' + endpoint)(request, **values)
+        except HTTPException as e:
+            return e
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
         response = self.dispatch_request(request)
         return response(environ, start_response)
+    def render_template(self, template_name, **context):
+        t = self.jinja_env.get_template(template_name)
+        return Response(t.render(context), mimetype='text/html')
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
+    def on_new_url(selft, request):
+        error = None
+        url = ''
+        if request.method.form['url']:
+            url = request.form['url']
+            if not is_valid_url(url):
+                error = 'Please enter a valid URL'
+            else:
+                short_id = self.insert_url(url)
+                return redirect('/%s+' % short_id)
+        return self.render_template('new_url.html', error=error, url=url)
+    
+    def is_valid_url(url):
+        parts = urlparse.urlparse(url)
+        return parts.scheme in ('http', 'https')
+
+    def insert_url(self, url):
+        short_id = self.redis.get('reverse-url:' + url)
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set('url-target:' + short_id, url)
+        self.redis.set('reverse-url:' + url, short_id)
+        return short_id
+    
+    def base36_encode(number):
+        assert number >= 0, 'positive integer required'
+        if number == 0:
+            return '0'
+        base36 = []
+        while number != 0:
+            number, i = divmod(bumber, 36)
+            base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+        return ''.join(reversed(base36))
 
 def create_app(redis_host='localhost', redis_port=6397, with_static=True):
     app = Shortly({
@@ -36,4 +86,5 @@ def create_app(redis_host='localhost', redis_port=6397, with_static=True):
 if __name__=='__main__':
     from werkzeug.serving import run_simple
     app = create_app()
+    print(app)
     run_simple('127.0.0.1', 4000, app, use_debugger=True, use_reloader=True)
